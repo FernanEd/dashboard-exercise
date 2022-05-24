4a9cc22f9f08aa836d58c9225ab5ea4e3857dbce
FernanEd <fernandoed.bm@gmail.com>
2022-05-23 23:16:06 -0500

    done navbar and sidebar, added resizing event listener

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { MdOutlineCancel } from "react-icons/md";
+import { links } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
+
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <SiShopware /> <span>Company</span>
+            </Link>
+
+            <TooltipComponent content="Close" position="BottomCenter">
+              <button
+                type="button"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+                onClick={handleCloseSideBar}
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+
+          <div className="flex flex-col">
+            {links.map((link) => (
+              <div key={link.title}>
+                <p className="text-gray-400 uppercase m-3 mt-4">{link.title}</p>
+                {link.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={handleCloseSideBar}
+                    className={({ isActive }) =>
+                      `flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2 ${
+                        isActive
+                          ? "text-white bg-blue-500 font-bold"
+                          : "text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray"
+                      }`
+                    }
+                  >
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+import { Stacked, Pie, Button, SparkLine } from "../components";
+import {
+  earningData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+
 const ECommerce = () => {
   return <div>ECommerce</div>;
 };
